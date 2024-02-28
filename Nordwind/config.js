6c7f28ec2fb2cc2c7fd9c3773c1ac972b9de9d4f
hilberto <josoflo@ibadia.cat>
2024-02-28 17:24:06 +0100

    Comienzo de la configuracio

--- Nordwind/config.js
+++ Nordwind/config.js
@@ -1,30 +1,39 @@
+const { Client } = require('pg')
+const express = require('express'); 
 
-
-class dataContext {
-    constructor() {
-        this.client = new Client({
-                user: 'postgres',
-                host: 'localhost',
-                database: 'Facturas',
-                password: 'alubbdd',
-                port: 5432
-        })
-
-        this.client.connect()
-    }
-
-  async  executeQuery(query) {
-    
-      try {
-            const resultado = await this.client.query(query)
-
-            console.log('Query ejecutada con exito')
-            return JSON.stringify(resultado.rows)
-
-            this.client.end()
-
-        } catch (error) {
-            console.log('Error en la query', error)
-        }
-    }
+const config = {
+            user: 'postgres',
+            host: 'localhost',
+            database: 'Nordwind',
+            password: 'alubbdd',
+            port: 5432
 }
+
+const client = new Client(config)
+
+client.connect();
+const app = express();
+const PORT = 3001;
+
+app.use('/', express.static('./public'));
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+   
+
+    try {
+        const result = await client.query("SELECT * FROM products");
+        const products = result.rows;
+        console.log(products);
+        res.json(products);
+    } catch (error) {
+        console.error('Error al obtener productos:', error);
+        res.status(500).send('Error interno del servidor');
+    } finally {
+        await client.end();
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
