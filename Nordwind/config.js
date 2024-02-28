d580efcf816143efb20a5e6a98597da443acd94c
hilberto <josoflo@ibadia.cat>
2024-02-28 16:15:09 +0100

    Comienzo de la configuracio

---
+++ Nordwind/config.js
@@ -0,0 +1,30 @@
+
+
+class dataContext {
+    constructor() {
+        this.client = new Client({
+                user: 'postgres',
+                host: 'localhost',
+                database: 'Facturas',
+                password: 'alubbdd',
+                port: 5432
+        })
+
+        this.client.connect()
+    }
+
+  async  executeQuery(query) {
+    
+      try {
+            const resultado = await this.client.query(query)
+
+            console.log('Query ejecutada con exito')
+            return JSON.stringify(resultado.rows)
+
+            this.client.end()
+
+        } catch (error) {
+            console.log('Error en la query', error)
+        }
+    }
+}
