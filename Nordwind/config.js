c59cc5716f07f6917f65b95db0a1866f8bece922
Mariama Tamba <mariamatamba453@gmail.com>
2024-03-04 17:00:29 +0100

    cambios

--- Nordwind/config.js
+++ Nordwind/config.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 const { Client } = require('pg');
 const express = require('express'); 
 require('dotenv').config(); // Carga las variables de entorno desde .env
-=======
-const { Client } = require('./node_modules/pg')
-const express = require('./node_modules/express'); 
->>>>>>> e1e887ddd961ae895384071d6263e51d6804c210
 
 const config = {
     user: process.env.USER,
@@ -15,11 +10,7 @@ const config = {
     port: process.env.PORT
 }
 
-<<<<<<< HEAD
 const client= new Client(config);
-=======
-const client = new Client(config);
->>>>>>> e1e887ddd961ae895384071d6263e51d6804c210
 
 client.connect();
 const app = express();
@@ -29,10 +20,6 @@ app.use('/', express.static('./public'));
 app.use(express.json());
 
 app.get('/products', async (req, res) => {
-<<<<<<< HEAD
-=======
-   
->>>>>>> e1e887ddd961ae895384071d6263e51d6804c210
     try {
         const result = await client.query("SELECT * FROM products");
         const products = result.rows;
@@ -40,19 +27,6 @@ app.get('/products', async (req, res) => {
         res.json(products);
     } catch (error) {
         console.error('Error al obtener productos:', error);
-        res.status(500).send('Error interno del servidor');
-    } finally {
-        await client.end();
-    }
-});
-app.get('/orders', async (req, res) => {
-   
-    try {
-        const result = await client.query("SELECT * FROM orders");
-        const orders = result.rows;
-        res.json(orders);
-    } catch (error) {
-        console.error('Error al obtener orders:', error);
         res.status(500).send('Error interno del servidor');
     } finally {
         await client.end();
