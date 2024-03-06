fe12aee1ddb3401c0347e4ed18641a4310a4d4c6
Mariama Tamba <mariamatamba453@gmail.com>
2024-03-06 15:42:16 +0100

    Cambios

--- Nordwind/config.js
+++ Nordwind/config.js
@@ -2,6 +2,8 @@ import pg from 'pg';
 const { Client } = pg;
 import express  from 'express'; 
 import ordersRoutes  from './routas/order.js';
+import empleadosRoutes  from './routas/empleado.js';
+
 
 import dotenv from 'dotenv';
 
@@ -23,7 +25,7 @@ const PORT = 3000;
 app.use('/', express.static('./public'));
 app.use(express.json());
 
-app.use('/empleados', require('routas/empleado.js'))
+app.use('/empleados', empleadosRoutes);
 
 app.get('/products', async (req, res) => {
    
