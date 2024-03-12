5c1b9672280ee4173ebc6d01882bfa3c194bc4e9
Mariama Tamba <mariamatamba453@gmail.com>
2024-03-12 10:27:17 +0100

    Cambios en el insert

--- Nordwind/controllers/empleados.js
+++ Nordwind/controllers/empleados.js
@@ -12,5 +12,31 @@ router.get('/', async (req, res) => {
     }
 });
 
+// Ruta para manejar la inserción de empleados
+router.post('/', async (req, res) => {
+  const { last_name, first_name, title, region, postal_code, home_phone, address, extension, reports_to, photo_path } = req.body;
+
+  try {
+      // Obtener todos los empleados excepto el que se está insertando
+       result = await client.query('SELECT * FROM employees WHERE employee_id != $1', [reports_to]);
+      const employees = result.rows;
+
+      // Verificar si el ID del reporte seleccionado existe en la lista de empleados disponibles
+      const isValidReportTo = employees.some(employee => employee.employee_id === reports_to);
+
+      if (!isValidReportTo) {
+          return res.status(400).json({ error: 'El empleado seleccionado como reporte no es válido' });
+      }
+
+      const client = await pool.connect();
+      const result = await client.query('INSERT INTO employees (last_name, first_name, title, region, postal_code, home_phone, address, extension, reports_to, photo_path) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)', 
+      [last_name, first_name, title, region, postal_code, home_phone, address, extension, reports_to, photo_path]);
+      client.release();
+      res.json({ message: 'Empleado insertado exitosamente' });
+  } catch (err) {
+      console.error('Error al insertar empleado:', err);
+      res.status(500).json({ error: 'Error al insertar empleado' });
+  }
+});
 export default router;
 import { client } from '../config.js';
